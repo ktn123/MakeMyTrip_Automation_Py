76e977c09a61269ff94e582a8abcaed6f9e4e698
Ketan <gharpureketan@gmail.com>
2020-09-06 22:44:48 +0530

    Error in login_box_appear method

--- Base/BaseModule.py
+++ Base/BaseModule.py
@@ -1,14 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class Base:
     global driver
+    global ex_wait
     def __init__(self):
         self.driver = webdriver.Chrome("C:\\Users\\Ketan\\eclipse-workspace-finalprojects\\MakeMyTrip_Automation_Py\\Executables\\chromedriver.exe")
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
-        print(self.driver)
+        self.ex_wait = WebDriverWait(self.driver,20)
+    
+    def login_box_appear(self):
+        try:
+            if self.ex_wait.until(EC.visibility_of_element_located((By.XPATH,"//p[text()='Login/Signup for Best Prices']"))):
+                visible=self.ex_wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"#stickyScroll")))
+                clickable = self.ex_wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#stickyScroll")))
+                print("-------",visible,clickable)
+                self.driver.find_element_by_css_selector("#stickyScroll").click()
+                self.ex_wait.until(EC.invisibility_of_element_located((By.XPATH,"//p[text()='Login/Signup for Best Prices']")))
+        except:
+            print("An Exception occured.")        
+    def wait_for_page_to_load(self):
+        #To wait for page to load(a specific element to load)
+        self.ex_wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"div#PROMOTIONS")))
+        self.login_box_appear()
+    
     def setUp(self):
         self.driver.get("https://www.makemytrip.com/")
+        self.wait_for_page_to_load()
+    
     def tearDown(self):
         self.driver.quit()
         
--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,11 +1,11 @@
 from Base.BaseModule import Base
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions as EC
+import time
 class Home(Base):
     #b = Base()
     
     def navigate_to_FightsMenu(self):
-        print(self.driver)
         self.driver.find_element(By.CSS_SELECTOR,".menu_Flights a").click()
     
     def navigate_to_CabsMenu(self):
@@ -20,14 +20,40 @@ class Home(Base):
     def navigate_to_BusesMenu(self):
         self.driver.find_element(By.CSS_SELECTOR,".menu_Cabs a").click()
     
-
-b.setUp()
-
-h = Home()
-h.navigate_to_FightsMenu()   
-h.navigate_to_BusesMenu()
-h.navigate_to_HotelsMenu()
-h.navigate_to_CabsMenu()
-h.navigate_to_TrainsMenu()
-
-b.tearDown()
+    def scroll_to_super_offers(self):
+        self.driver.execute_script("window.scrollTo(0, 400)") 
+        self.ex_wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"div#Offers_Listing")),"Element is Present now.")
+        
+    def scroll_to_travel_blog(self):
+        self.driver.execute_script("window.scrollTo(0,1100)")
+        self.ex_wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"div.travelBlog")))
+        
+    def change_from_booking_country(self,country):
+        self.ex_wait.until(EC.element_to_be_clickable((By.CLASS_NAME,"ctrySelect")), "Element is now clickable.")
+        self.driver.find_element_by_class_name("ctrySelect").click()
+        self.ex_wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,".ctryList p.ctryListItem:nth-child(1)")))
+            
+        if country.lower() == "india":
+            self.driver.find_element_by_css_selector(".ctryList p.ctryListItem:nth-child(1)").click()
+        elif country.lower() == "uae":
+            self.driver.find_element_by_css_selector(".ctryList p.ctryListItem:nth-child(2)").click()
+        elif country.lower() == "us":
+            self.driver.find_element_by_css_selector(".ctryList p.ctryListItem:nth-child(3)").click()
+        else:
+            print("Country no found in the list")
+        #time.sleep(10)
+        self.wait_for_page_to_load()
+        
+    def return_back_to_mmt_homepage(self):
+        self.driver.find_element_by_css_selector(".mmtLogo").click()
+        self.wait_for_page_to_load()
+try:
+    h = Home()
+    h.setUp()
+    h.change_from_booking_country("us")
+finally:    
+    h.tearDown()
+    
+    
+    
+    
