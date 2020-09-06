480a5d062029f95fbf1817ec94afaef087d9e231
Ketan <gharpureketan@gmail.com>
2020-09-06 13:22:58 +0530

    Working on Home Page

--- Base/BaseModule.py
+++ Base/BaseModule.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 class Base:
     global driver
     def __init__(self):
-        self.driver = webdriver.Chrome("C:\\Users\\Ketan\\eclipse-workspace-finalprojects\\FlightBooking_Automation\\Executables\\chromedriver.exe")
+        self.driver = webdriver.Chrome("C:\\Users\\Ketan\\eclipse-workspace-finalprojects\\MakeMyTrip_Automation_Py\\Executables\\chromedriver.exe")
         self.driver.implicitly_wait(20)
         self.driver.maximize_window()
+        print(self.driver)
     def setUp(self):
         self.driver.get("https://www.makemytrip.com/")
     def tearDown(self):
@@ -13,11 +14,6 @@ class Base:
         
     #WebDriver methods will be declared here
     
-    
-    
-b = Base()
-b.setUp()
-b.tearDown()
 
 
 
--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,2 +1,33 @@
-from Base import BaseModule
+from Base.BaseModule import Base
+from selenium.webdriver.common.by import By
+
 class Home(Base):
+    #b = Base()
+    
+    def navigate_to_FightsMenu(self):
+        print(self.driver)
+        self.driver.find_element(By.CSS_SELECTOR,".menu_Flights a").click()
+    
+    def navigate_to_CabsMenu(self):
+        self.driver.find_element(By.CSS_SELECTOR,".menu_Hotels a").click()
+    
+    def navigate_to_HotelsMenu(self):
+        self.driver.find_element(By.CSS_SELECTOR,".menu_Trains a").click()
+    
+    def navigate_to_TrainsMenu(self):
+        self.driver.find_element(By.CSS_SELECTOR,".menu_Buses a").click()
+        
+    def navigate_to_BusesMenu(self):
+        self.driver.find_element(By.CSS_SELECTOR,".menu_Cabs a").click()
+    
+
+b.setUp()
+
+h = Home()
+h.navigate_to_FightsMenu()   
+h.navigate_to_BusesMenu()
+h.navigate_to_HotelsMenu()
+h.navigate_to_CabsMenu()
+h.navigate_to_TrainsMenu()
+
+b.tearDown()
